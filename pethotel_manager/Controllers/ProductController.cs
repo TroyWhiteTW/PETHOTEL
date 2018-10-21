fd850a8648bd20df7331d1b1544d7f5496fa2a85
KAI <dciyi9cum6rh@gmail.com>
2018-10-21 12:16:32 +0800

    產品新增

--- pethotel_manager/Controllers/ProductController.cs
+++ pethotel_manager/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pethotel_manager.Models;
 
 namespace pethotel_manager.Controllers
 {
@@ -13,5 +14,20 @@ namespace pethotel_manager.Controllers
         {
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ProductViewModel vm)
+        {
+            vm.create();
+            TempData["ResultMessage"] = String.Format("商品[{0}]成功建立", vm.p_name);
+
+            //轉導Product/Index頁面
+            return RedirectToAction("Create");
+        }
+
     }
 }
---
+++ pethotel_manager/Models/ProductViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using pethotel_manager.Entity;
+using System.Web.Mvc;
+namespace pethotel_manager.Models
+{
+    public class ProductViewModel
+    {
+        public int p_id { get; set; }
+        public Nullable<int> p_type { get; set; }
+        public string p_name { get; set; }
+        public string p_content { get; set; }
+        public Nullable<decimal> p_price { get; set; }
+        public Nullable<int> p_count { get; set; }
+        public string p_image { get; set; }
+
+
+        public void create()
+        {
+
+        
+            Entities db = new Entity.Entities();
+            Product p = new Product();
+            p.p_name = this.p_name;
+            p.p_type = this.p_type;
+            p.p_count = this.p_count;
+            p.p_price = this.p_price;
+            p.p_content = this.p_content;
+            p.p_image = this.p_image;
+            db.Product.Add(p);
+            db.SaveChanges();
+            
+
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+    
+
+}

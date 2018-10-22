8334d6eabdcca8173657ab94825d08e8f2817e55
Jimmy <andy08055@gmail.com>
2018-10-22 10:52:13 +0800

    後端 pethotel_manager     product 新增 刪除 修改 完成 剩下 create 的 上傳圖片到商品列表和資料庫尚未處理好

--- pethotel_manager/Controllers/ProductController.cs
+++ pethotel_manager/Controllers/ProductController.cs
@@ -1,17 +1,90 @@
-﻿using System;
+﻿using pethotel_manager.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using pethotel_manager.Entity;
 
 namespace pethotel_manager.Controllers
 {
     public class ProductController : Controller
     {
+        Entities db = new Entities();
         // GET: Product
         public ActionResult Index()
         {
+            Entities db = new Entities();
+            var query = from o in db.Product select o;
+            var dataList = query.ToList();
+            ViewBag.p = dataList;
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(ProductViewModel VM)
+        {
+
+            VM.create();
+
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using ( Entities db = new Entities())
+            {
+                var result = (from s in db.Product where s.p_id == id select s).FirstOrDefault();
+
+
+                return View(result);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Models.ProductViewModel postback)
+        {
+            var query = from o in db.Product
+                        where o.p_id == postback.p_id
+                        select o;
+
+            var pro = query.FirstOrDefault();
+            pro.p_id = postback.p_id;
+            pro.p_name = postback.p_name;
+            pro.p_content = postback.p_content;
+            pro.p_price = postback.p_price;
+            pro.p_count = postback.p_count;
+            pro.p_image = postback.p_image;
+           
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            using (Entities db = new Entities())
+            {
+                var result = (from s in db.Product where s.p_id == id select s).FirstOrDefault();
+                
+                
+                    db.Product.Remove(result);
+
+                    db.SaveChanges();
+                    
+                    return RedirectToAction("Index");                               
+            }
+
+
+
+
+
+
+        }
+
     }
 }
---
+++ pethotel_manager/Models/ProductViewModel.cs
@@ -0,0 +1,37 @@
+﻿using pethotel_manager.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pethotel_manager.Models
+{
+    public class ProductViewModel
+    {
+        public int p_id { get; set; }
+        public int p_type { get; set; }
+        public string p_name { get; set; }
+        public string p_content { get; set; }
+        public int p_price { get; set; }
+        public int p_count { get; set; }
+        public string p_image { get; set; }
+
+
+
+        public void create()
+        {
+            Entities en = new Entities();
+            Product item = new Product();
+            item.p_type = this.p_type;
+            item.p_name = this.p_name;
+            item.p_content = this.p_content;
+            item.p_price = this.p_price;
+            item.p_count = this.p_count;
+            item.p_image = this.p_image;
+
+            en.Product.Add(item);
+            en.SaveChanges();         
+        }
+     
+    }
+}

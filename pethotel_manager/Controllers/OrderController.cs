22fb4154349c9eb8a698dc4a29816290e7c7a5b9
DESKTOP-LFQBBVN\user <tou19880321@gmail.com>
2018-10-23 15:45:15 +0800

    order控制器

---
+++ pethotel_manager/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pethotel_manager.Models;
+
+namespace pethotel_manager.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(OrderViewModel VM)
+        {
+
+            VM.create();
+
+            return View();
+        }
+
+
+    }
+}
